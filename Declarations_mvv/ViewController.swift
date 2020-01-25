fa49d46812152405a6bfe53e0fe193746f967717
Vadik_mvv <mvvvadik@gmail.com>
2020-01-25 22:54:42 +0200

    [Add]Initial Commit

---
+++ Declarations_mvv/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Declarations_mvv
+//
+//  Created by vadik on 1/25/20.
+//  Copyright © 2020 vadik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
